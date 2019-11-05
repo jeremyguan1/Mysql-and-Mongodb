ecb6becf9cf35e3f96f76d7665ff79a50dc912fd
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-04 16:57:56 -0800

    updating

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
-const getDb = require("../util/database").getDb;
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
 class Product {
   constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
@@ -11,12 +12,15 @@ class Product {
   }
 
   save() {
-    let db = getDb();
+    const db = getDb();
     let dbOp;
     if (this._id) {
-      dbOp = db.collection("products").updateOne({ _id: this._id }, { $set: this });
+      // Update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
-      dbOp = db.collection("products").insertOne(this);
+      dbOp = db.collection('products').insertOne(this);
     }
     return dbOp
       .then(result => {
@@ -30,35 +34,44 @@ class Product {
   static fetchAll() {
     const db = getDb();
     return db
-      .collection("products")
+      .collection('products')
       .find()
       .toArray()
       .then(products => {
         console.log(products);
         return products;
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static findById(prodId) {
     const db = getDb();
     return db
-      .collection("products")
+      .collection('products')
       .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
-      .then(product => product)
-      .catch(err => err);
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static deleteById(prodId) {
     const db = getDb();
     return db
-      .collection("products")
+      .collection('products')
       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
       .then(result => {
-        console.log("Delete");
+        console.log('Deleted');
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
